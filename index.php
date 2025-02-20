12401a6aa7d95f565e597a9229ec4390264b8252
Marlon Progetti <marlop.dossantos@gmail.com>
2025-02-20 18:01:23 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0
+  ">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/pagina-de-login.png">
+  <title>Login</title>
+</head>
+
+<body>
+  <div class="login">
+    <div class="form">
+      <img class="login_logo" src="img/arrastao-logo.jpg" alt="
+      logo arrastao">
+      <form>
+        <input type="email" name="usuario" placeholder="Seu Email">
+        <input type="password" name="senha" placeholder="Senha">
+        <button>Login</button>
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
