7a75267e052d29a812018695ee5c8e58bd8e4a5c
Marlon Progetti <marlop.dossantos@gmail.com>
2025-02-26 20:24:54 -0300

    Criando a segurança do nosso site e também iniciando a pagina de pós login e logout

--- autenticar.php
+++ autenticar.php
@@ -1,4 +1,7 @@
 <?php 
+
+@session_start();
+
 require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
@@ -11,7 +14,10 @@ $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = count($res);
 
 if($linhas > 0){
-  echo 'Login permitido!';
+  $_SESSION['nome'] = $res[0]['nome'];
+  $_SESSION['id'] = $res[0]['id'];
+  $_SESSION['nivel'] = $res[0]['nivel'];
+  echo '<script>window.location="painel"</script>';
 }else{
   echo '<script>window.alert("Dados Incorretos!!")</script>';
   echo '<script>window.location="index.php"</script>';
